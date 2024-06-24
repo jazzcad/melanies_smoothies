b21c04f7a15af74069f7fd4383ee50439958c80a
jazzcad <jazzcad@hotmail.com>
2024-06-24 16:33:53 -0400

    Update streamlit_app.py
    add import requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,8 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered,' + ' ' + name_on_order, icon="✅")
 
+#new section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
